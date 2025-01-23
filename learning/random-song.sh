f7466c15a5f41dd713e7e813da2f0ff4de66089a
Aidan Houck <AidanHouck@users.noreply.github.com>
2025-01-23 20:23:58 +0000

    feat: add `random-song.sh`

---
+++ learning/random-song.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Retrieve a random song from Apple's iTunes API
+# https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
+
+load() {
+	# Echo, find key pair, strip to just value, remove leading and trailing "'s
+	echo "$output" | grep -o "$1"'":"[^"]*"' | cut -d':' -f2- | sed 's/^.\|.$//g'
+}
+
+# random number
+high=3
+low=1
+randNum=$(python3 -c "import random; print(random.randint($low, $high))")
+
+# random 2 letters
+rand=$(echo "$RANDOM" | md5sum | head -c "$randNum")
+
+output=$(curl -s 'https://itunes.apple.com/search?term='"$rand"'&media=music&entity=song&limit=1')
+
+# this doesn't work since I can't know the range that iTunes uses
+#curl -s 'https://itunes.apple.com/lookup?id='"$rand"
+
+if [[ $(load 'artistName') == "" ]]; then
+	# No response recieved from API, try again
+	"$0"
+	exit 1
+fi
+
+echo -n "Artist name: " && load 'artistName'
+echo -n "Song name: " && load 'trackName'
+echo -n "Song genre: " && load 'primaryGenreName'
+echo -n "iTunes link: " && load 'trackViewUrl'
+
