9dd12bba2b2bbfccec7563c7907d186bb7e89470
Aidan Houck <AidanHouck@users.noreply.github.com>
2025-08-21 10:12:08 -0400

    feat: git fetch before showing status

--- utility/tmux-work-session.sh
+++ utility/tmux-work-session.sh
@@ -10,10 +10,10 @@ if [ $? != 0 ]; then
 	tmux new-session -s $NAME -d \; send-keys 'cd ~/ssh.d && ls' C-m
 
 	# Top-right
-	tmux split-window -h \; send-keys 'cd ~/src/nix-config && git status' C-m \; rename-window "main"
+	tmux split-window -h \; send-keys 'cd ~/src/nix-config && git fetch && git status' C-m \; rename-window "main"
 
 	# Bottom-right
-	tmux split-window -v \; send-keys 'cd ~/scripts/work/palo && git status && ./palo-api-key.sh' C-m
+	tmux split-window -v \; send-keys 'cd ~/scripts/work/palo && git fetch && git status && ./palo-api-key.sh' C-m
 
 	# Bottom-left
 	tmux select-pane -L
