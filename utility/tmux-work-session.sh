72c6b69a574626e88a3fa3f5eb3c3bc1c7ab2e22
Aidan Houck <AidanHouck@users.noreply.github.com>
2025-10-03 11:49:49 -0400

    chore: lowercase dirs on share drive

--- utility/tmux-work-session.sh
+++ utility/tmux-work-session.sh
@@ -14,7 +14,7 @@ if [ $? != 0 ]; then
 	tmux split-window -h \; send-keys 'cd ~/src/nix-config && git fetch && git status' C-m \; rename-window "main"
 
 	# Right column
-	# tmux split-window -h \; send-keys 'cd /mnt/o/Aidan/Notes && vim r_todo.txt' C-m
+	# tmux split-window -h \; send-keys 'cd /mnt/o/aidan/notes && vim r_todo.txt' C-m
 
 	# Bottom-middle
 	#tmux select-pane -L
@@ -22,7 +22,7 @@ if [ $? != 0 ]; then
 
 	# Bottom-left
 	tmux select-pane -L
-	tmux split-window -v \; send-keys 'cd /mnt/o/Aidan/Notes && tree -dL 2' C-m
+	tmux split-window -v \; send-keys 'cd /mnt/o/aidan/notes && tree -dL 2' C-m
 
 	# Set sizing
 	#tmux select-layout 'e3ed,211x54,0,0{83x54,0,0[83x26,0,0,0,83x27,0,27,4],83x54,84,0[83x26,84,0,1,83x27,84,27,3],43x54,168,0,2}'
